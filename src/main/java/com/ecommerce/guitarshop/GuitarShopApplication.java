5214494d30e677a36e83506ac3b783525336c1ed
NishChal370 <nishchal.370@gmail.com>
2021-10-13 06:32:40 +0545

    project setup done

---
+++ src/main/java/com/ecommerce/guitarshop/GuitarShopApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.guitarshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarShopApplication.class, args);
+	}
+
+}
